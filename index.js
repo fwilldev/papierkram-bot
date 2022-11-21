0707d6b29b1789b43bc62830a883d794d05c193c
Fabian Will <fabian2911@gmail.com>
2022-11-22 00:32:27 +0100

    improvements

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const puppeteer = require('puppeteer');
 const moment = require('moment');
-
+const prompt = require('prompt-sync')();
+const isValidUrl = urlString => {
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+        '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+    return !!urlPattern.test(urlString);
+}
 (async () => {
     moment.locale('de')
     const firstDate = moment(process.argv[2], 'DD/MM/YYYY'); // start date
@@ -10,8 +19,13 @@ const moment = require('moment');
     const subject = process.argv[6]; // Subject you want to book
     const email = process.argv[7];
     const password = process.argv[8];
-    const url = process.argv[9];
+    let url = process.argv[9];
     var result = [moment({...firstDate})];
+
+    if (!isValidUrl(url) && !url.includes("papierkram.de")) {
+        console.error("Eingabe URL ist nicht valide: ", url);
+        return;
+    }
 
     while(lastDate.date() !== firstDate.date()){
         firstDate.add(1, 'day');
@@ -26,19 +40,22 @@ const moment = require('moment');
             const dateString = moment(day, "DD/MM/YYYY").format("DD/MM/YYYY");
             dates.push(dateString.split("/").join("."));
         }
+
     })
 
     console.log('Auf folgende Tage wird gebucht: ', dates);
-
     const browser = await puppeteer.launch({
         headless: true});
     const page = await browser.newPage();
+
     await page.setViewport({
         width: 1920,
         height: 1080,
         deviceScaleFactor: 1,
     });
-    await page.goto(url + '/login', {waitUntil: "networkidle2"});
+    const loginUrl = url.endsWith("/") ? url + "login" : url + '/login';
+    console.log("Logge ein in: ", url);
+    await page.goto(loginUrl, {waitUntil: "networkidle2"});
     await page.type('#user_new_email', email, {delay: 100});
     await page.type('#user_new_password', password, {delay: 100});
     await page.click('input[name="commit"]', {delay: 500});
@@ -46,6 +63,8 @@ const moment = require('moment');
     await page.waitForSelector('.user-name');
     await page.waitForSelector('i.icon-pk-tracker');
     await page.goto(url + '/zeiterfassung/buchungen?b=&show_new_form=true', {waitUntil: "networkidle2"});
+    let selectedProject = "";
+    let projectElement = undefined;
 
     for (const day of dates) {
         await page.waitForSelector('#s2id_tracker_time_entry_new_complete_project_id');
@@ -53,12 +72,28 @@ const moment = require('moment');
         await page.waitForTimeout(1000);
         let elementHandle = await page.$x(`//div[@class='select2-result-label']`);
 
-        console.log('Wähle letztes Projekt aus.');
+        if (selectedProject.length === 0) {
+            console.log("Verfügbare Projekte: ");
+            for (const e of elementHandle) {
+                const elementText = await page.evaluate(el => el.textContent, e);
+                console.log(elementText);
+            }
+            selectedProject = prompt('Gebe den Namen des Projektes ein: ').trim();
+            for (const e of elementHandle) {
+                const elementText = await page.evaluate(el => el.textContent, e);
+                if (elementText === selectedProject) {
+                    console.log("Buche auf Projekt: ", selectedProject);
+                    projectElement = e;
+                    break;
+                }
+            }
+        }
 
-        if (elementHandle.length > 0) {
-            await elementHandle[elementHandle.length - 1].click({delay: 200})
+        if (projectElement) {
+            await projectElement.click({delay: 200})
         } else {
-            throw new Error("Project not found");
+            await browser.close();
+            throw new Error("Project nicht gefunden: " + selectedProject);
         }
 
         await page.type('#tracker_time_entry_new_complete_task_name', subject, {delay: 100});
@@ -76,8 +111,9 @@ const moment = require('moment');
         await page.waitForTimeout(1000);
         await page.goto(url + '/zeiterfassung/buchungen?b=&show_new_form=true', {waitUntil: "networkidle2"});
     }
-
+    const controlUrl = url + '/zeiterfassung/buchungen?t=' + moment(firstDate, 'YYYY-MM-DD') + '..' +  moment(lastDate, 'YYYY-MM-DD');
     await page.waitForSelector('.logout');
     await page.click('.logout', {delay: 1000});
+    console.log('Logout erfolgreich. Bitte Zeiten kontrollieren: ' + controlUrl);
     await browser.close();
 })();
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Papierkram Bot V1"
 echo "--------------------------------"
 echo "Automatisches Zeiten buchen vom Start Datum bis End Datum - Wochenenden ausgeschlossen."
-read -p 'Papierkram URL mit https, ohne / am Ende: ' urlvar
+read -p 'Papierkram URL: ' urlvar
 read -p 'Start Datum (Format: DD.MM.YYYY): ' startdatevar
 read -p 'End Datum (Format: DD.MM.YYYY): ' enddatevar
 read -p 'Start Zeit: (Format: HH:MM) ' starttimevar
@@ -16,13 +16,9 @@ then
       echo "Ein Argument ist leer. Bitte alle Argumente ausfüllen!"
       exit 0
 fi
-DIR="node_modules"
 
-if [ ! -d "$DIR" ]; then
-    echo "Node Modules existieren nicht. Werden installiert: "
-    npm install
-fi
-
+echo "Prüfe und installiere Dependencies: "
+npm install
 
 node index.js $startdatevar $enddatevar $starttimevar $endtimevar $descriptionvar $emailvar $passwortvar $urlvar
-echo "Script durchgelaufen. Bitte Zeiten nochmal kontrollieren!"
+echo "Script durchgelaufen."
