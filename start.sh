602e9ca2f3dc1c4f09902bae3ce8a067cf086d5a
Christof Flick <c.flick@awesome-software.de>
2022-12-19 15:06:53 +0100

    optimize bash script

--- start.sh
+++ start.sh
@@ -1,43 +1,36 @@
 echo "Papierkram Bot V1"
 echo "--------------------------------"
 echo "Automatisch Zeiten buchen vom Start Datum bis End Datum - Wochenenden ausgeschlossen."
-if [ ! -f history.json ];
-then
-  read -p 'Papierkram URL: ' urlvar
-else
-  urlvar=$(grep -o '"url": "[^"]*' history.json | grep -o '[^"]*$')
-fi
+
 read -p 'Start Datum (Format: DD.MM.YYYY): ' startdatevar
 read -p 'End Datum (Format: DD.MM.YYYY): ' enddatevar
 read -p 'Start Zeit: (Format: HH:MM) ' starttimevar
 read -p 'End Zeit: (Format: HH:MM) ' endtimevar
-if [ ! -f history.json ];
-then
-  read -p 'Tätigkeitsbeschreibung: (zB Entwicklung) ' descriptionvar
-else
+
+if [ -f history.json ]; then
+  urlvar=$(grep -o '"url": "[^"]*' history.json | grep -o '[^"]*$')
   descriptionvar=$(grep -o '"subject": "[^"]*' history.json | grep -o '[^"]*$')
-fi
-if [ ! -f history.json ];
-then
-  read -p 'Email Adresse: ' emailvar
-else
   emailvar=$(grep -o '"email": "[^"]*' history.json | grep -o '[^"]*$')
   echo "Folgende E-Mail wird verwendet:" $emailvar
+else
+  read -p 'Papierkram URL: ' urlvar
+  read -p 'Tätigkeitsbeschreibung: (zB Entwicklung) ' descriptionvar
+  read -p 'Email Adresse: ' emailvar
 fi
+
 echo 'Passwort: '
 read -s passwortvar
 
-if test -z "$startdatevar" || test -z "$enddatevar" || test -z "$starttimevar" || test -z "$endtimevar" || test -z "$startdatevar" || test -z "$descriptionvar" || test -z "$emailvar" || test -z "$passwortvar"
-then
+if test -z "$startdatevar" || test -z "$enddatevar" || test -z "$starttimevar" || test -z "$endtimevar" || test -z "$startdatevar" || test -z "$descriptionvar" || test -z "$emailvar" || test -z "$passwortvar"; then
       echo "Ein Argument ist leer. Bitte alle Argumente ausfüllen!"
       exit 0
 fi
 
-DIR="node_modules"
-if [ ! -d "$DIR" ]; then
+
+if [ ! -d "node_modules" ]; then
     echo "Node Modules existieren nicht. Werden installiert: "
     npm install
 fi
 
-node index.js $startdatevar $enddatevar $starttimevar $endtimevar "$descriptionvar" $emailvar $passwortvar $urlvar
+node index.js "$startdatevar" "$enddatevar" "$starttimevar" "$endtimevar" "$descriptionvar" "$emailvar" "$passwortvar" "$urlvar"
 echo "Script durchgelaufen."
