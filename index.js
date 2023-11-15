9dfee967543f7467edc2b2ffa51426b2592f4b7d
fwilldev <mail@fwill.dev>
2023-11-15 16:03:00 +0100

    Add semicolons at the end of statements with prettier

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import puppeteer from 'puppeteer'
-import moment from 'moment'
-import history from './history.js'
-import regionCodes from './regioncodes.js'
-import { isHoliday } from 'feiertagejs'
-import keychain from 'keychain'
-import { confirm, password, select } from '@inquirer/prompts'
+import puppeteer from 'puppeteer';
+import moment from 'moment';
+import history from './history.js';
+import regionCodes from './regioncodes.js';
+import { isHoliday } from 'feiertagejs';
+import keychain from 'keychain';
+import { confirm, password, select } from '@inquirer/prompts';
 
 const isValidUrl = (urlString) => {
   const urlPattern = new RegExp(
@@ -17,9 +17,9 @@ const isValidUrl = (urlString) => {
       '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
       '(\\#[-a-z\\d_]*)?$',
     'i'
-  ) // validate fragment locator
-  return !!urlPattern.test(urlString)
-}
+  ); // validate fragment locator
+  return !!urlPattern.test(urlString);
+};
 
 const getPassword = (email) =>
   new Promise((resolve, reject) => {
@@ -31,92 +31,92 @@ const getPassword = (email) =>
       },
       (err, pw) => {
         if (err) {
-          reject(err)
+          reject(err);
         } else if (pw) {
-          resolve(pw)
+          resolve(pw);
         } else {
-          resolve(null)
+          resolve(null);
         }
       }
-    )
-  })
+    );
+  });
 
-;(async () => {
-  moment.locale('de')
+(async () => {
+  moment.locale('de');
 
-  let firstDate = moment(process.argv[2], 'DD/MM/YYYY') // start date
-  let lastDate = moment(process.argv[3], 'DD/MM/YYYY') // end date
-  let firstTime = process.argv[4] // start time
-  let lastTime = process.argv[5] // end time
+  let firstDate = moment(process.argv[2], 'DD/MM/YYYY'); // start date
+  let lastDate = moment(process.argv[3], 'DD/MM/YYYY'); // end date
+  let firstTime = process.argv[4]; // start time
+  let lastTime = process.argv[5]; // end time
 
-  const subject = process.argv[6] // Subject you want to book
-  history.set('subject', subject)
-  const email = process.argv[7]
-  history.set('email', email)
+  const subject = process.argv[6]; // Subject you want to book
+  history.set('subject', subject);
+  const email = process.argv[7];
+  history.set('email', email);
 
-  let url = process.argv[8]
-  const useMonth = process.argv[9].toLowerCase() === 'y'
-  const dryRun = process.argv[10] === 'true' // if true the bot will not book any times
+  let url = process.argv[8];
+  const useMonth = process.argv[9].toLowerCase() === 'y';
+  const dryRun = process.argv[10] === 'true'; // if true the bot will not book any times
 
-  const isMacOs = process.platform === 'darwin'
-  let pw
-  let isNewPassword = true
+  const isMacOs = process.platform === 'darwin';
+  let pw;
+  let isNewPassword = true;
   if (isMacOs) {
     const useKeychain = await confirm({
       message: 'Passwort aus Schlüsselbund verwenden? ',
       default: false,
-    })
+    });
     if (useKeychain) {
       try {
-        pw = await getPassword(email)
-        isNewPassword = false
+        pw = await getPassword(email);
+        isNewPassword = false;
       } catch (_) {
         console.log(
           'Kein Passwort für Papierkram-Bot im Schlüsselbund gefunden. '
-        )
-        isNewPassword = true
+        );
+        isNewPassword = true;
       }
     }
   }
 
   if (!pw) {
-    pw = await password({ message: 'Passwort: ', mask: true })
-    isNewPassword = true
+    pw = await password({ message: 'Passwort: ', mask: true });
+    isNewPassword = true;
   }
 
   if (isMacOs && isNewPassword) {
     const saveInKeychain = await confirm({
       message: 'Passwort im Schlüsselbund speichern? ',
       default: false,
-    })
+    });
     if (saveInKeychain) {
       keychain.setPassword({
         account: email,
         service: 'Papierkram Credentials',
         type: 'internet',
         password: pw,
-      })
+      });
     }
   }
 
   if (dryRun) {
-    console.log('Testlauf aktiviert. Es werden keine Zeiten gebucht.')
+    console.log('Testlauf aktiviert. Es werden keine Zeiten gebucht.');
   }
 
-  const firstDateForUrl = firstDate.format('YYYY-MM-DD')
-  const lastDateForUrl = lastDate.format('YYYY-MM-DD')
+  const firstDateForUrl = firstDate.format('YYYY-MM-DD');
+  const lastDateForUrl = lastDate.format('YYYY-MM-DD');
 
   if (!isValidUrl(url) && !url.includes('papierkram.de')) {
-    console.error('Eingabe URL ist nicht valide: ', url)
-    return
+    console.error('Eingabe URL ist nicht valide: ', url);
+    return;
   }
 
-  history.set('url', url)
-  history.save()
+  history.set('url', url);
+  history.save();
 
-  let regionChoices = []
+  let regionChoices = [];
   for (const [regionName, regionKey] of Object.entries(regionCodes)) {
-    regionChoices.push({ name: regionKey, value: regionName })
+    regionChoices.push({ name: regionKey, value: regionName });
   }
 
   const selectedRegion = await select({
@@ -124,140 +124,140 @@ const getPassword = (email) =>
       'Von welchem Bundesland sollen die Feiertage berücksichtigt werden? ',
     choices: regionChoices,
     pageSize: 26,
-  })
+  });
 
   if (useMonth) {
-    const currentDate = moment()
-    firstDate = currentDate.clone().startOf('month')
-    lastDate = currentDate.clone().endOf('month')
+    const currentDate = moment();
+    firstDate = currentDate.clone().startOf('month');
+    lastDate = currentDate.clone().endOf('month');
 
-    console.log(`Buchung für ${currentDate.format('MMMM YYYY')}`)
-    console.log(`Erster Tag: ${firstDate.format('DD.MM.YYYY')} ${firstTime}`)
-    console.log(`Letzter Tag: ${lastDate.format('DD.MM.YYYY')} ${lastTime}`)
+    console.log(`Buchung für ${currentDate.format('MMMM YYYY')}`);
+    console.log(`Erster Tag: ${firstDate.format('DD.MM.YYYY')} ${firstTime}`);
+    console.log(`Letzter Tag: ${lastDate.format('DD.MM.YYYY')} ${lastTime}`);
   }
 
-  let result = [moment({ ...firstDate })]
+  let result = [moment({ ...firstDate })];
 
   while (lastDate.date() !== firstDate.date()) {
-    firstDate.add(1, 'day')
-    result.push(moment({ ...firstDate }))
+    firstDate.add(1, 'day');
+    result.push(moment({ ...firstDate }));
   }
 
   const validDates = result
     .filter((day) => {
-      const isSaturday = day.toDate().getDay() === 6
-      const isSunday = day.toDate().getDay() === 0
+      const isSaturday = day.toDate().getDay() === 6;
+      const isSunday = day.toDate().getDay() === 0;
       return (
         !isSaturday && !isSunday && !isHoliday(day.toDate(), selectedRegion)
-      )
+      );
     })
-    .map((day) => moment(day).format('DD.MM.YYYY'))
+    .map((day) => moment(day).format('DD.MM.YYYY'));
 
   if (!validDates.length) {
     console.log(
       'In der angegebenen Zeitspanne wurden keine validen Tage gefunden.'
-    )
-    return
+    );
+    return;
   }
 
-  console.log('Auf folgende Tage wird gebucht: ', validDates)
+  console.log('Auf folgende Tage wird gebucht: ', validDates);
   const browser = await puppeteer.launch({
     headless: true,
-  })
-  const page = await browser.newPage()
+  });
+  const page = await browser.newPage();
 
   await page.setViewport({
     width: 1920,
     height: 1080,
     deviceScaleFactor: 1,
-  })
-  const correctUrl = url.trim().endsWith('/') ? url : url + '/'
-  console.log(`Logge ein in: ${correctUrl}`)
-  await page.goto(`${correctUrl}login`, { waitUntil: 'networkidle2' })
-  await page.type('#user_new_email', email, { delay: 100 })
-  await page.type('#user_new_password', pw, { delay: 100 })
-  await page.click('input[name="commit"]', { delay: 500 })
-  await page.waitForSelector('.user-name')
-  await page.waitForSelector('i.icon-pk-tracker')
-  console.log(`Login erfolgreich für User: ${email}`)
+  });
+  const correctUrl = url.trim().endsWith('/') ? url : url + '/';
+  console.log(`Logge ein in: ${correctUrl}`);
+  await page.goto(`${correctUrl}login`, { waitUntil: 'networkidle2' });
+  await page.type('#user_new_email', email, { delay: 100 });
+  await page.type('#user_new_password', pw, { delay: 100 });
+  await page.click('input[name="commit"]', { delay: 500 });
+  await page.waitForSelector('.user-name');
+  await page.waitForSelector('i.icon-pk-tracker');
+  console.log(`Login erfolgreich für User: ${email}`);
   await page.goto(
     correctUrl + 'zeiterfassung/buchungen?b=&show_new_form=true',
     { waitUntil: 'networkidle2' }
-  )
-  let selectedProject = ''
-  let projectElement
+  );
+  let selectedProject = '';
+  let projectElement;
   for (const day of validDates) {
     await page.waitForSelector(
       '#s2id_tracker_time_entry_new_complete_project_id'
-    )
-    await page.click('#s2id_tracker_time_entry_new_complete_project_id')
-    await new Promise((resolve) => setTimeout(resolve, 1000))
+    );
+    await page.click('#s2id_tracker_time_entry_new_complete_project_id');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     let elementHandle = await page.$$(
       '.select2-results-dept-1 .select2-result-label'
-    )
+    );
 
     if (selectedProject.length === 0) {
-      console.log('Verfügbare Projekte: ')
+      console.log('Verfügbare Projekte: ');
 
-      let projectChoices = []
+      let projectChoices = [];
       for (const el of elementHandle) {
-        const val = await page.evaluate((e) => e.textContent, el)
-        projectChoices.push({ name: val, value: val })
+        const val = await page.evaluate((e) => e.textContent, el);
+        projectChoices.push({ name: val, value: val });
       }
 
       selectedProject = await select({
         message: 'Auf welches Projekt soll gebucht werden? ',
         choices: projectChoices,
         pageSize: 26,
-      })
+      });
     }
 
     for (const e of elementHandle) {
-      const elementText = await page.evaluate((el) => el.textContent, e)
+      const elementText = await page.evaluate((el) => el.textContent, e);
       if (elementText === selectedProject) {
-        console.log('Buche auf Projekt: ', selectedProject)
-        projectElement = e
+        console.log('Buche auf Projekt: ', selectedProject);
+        projectElement = e;
       }
     }
 
     if (projectElement) {
-      await projectElement.click({ delay: 200 })
+      await projectElement.click({ delay: 200 });
     } else {
-      await browser.close()
-      throw new Error('Projekt nicht gefunden: ' + selectedProject)
+      await browser.close();
+      throw new Error('Projekt nicht gefunden: ' + selectedProject);
     }
     if (!dryRun) {
       await page.type('#tracker_time_entry_new_complete_task_name', subject, {
         delay: 100,
-      })
+      });
 
       await page.click('#tracker_time_entry_new_entry_date_f', {
         clickCount: 3,
-      })
+      });
       await page.type('#tracker_time_entry_new_entry_date_f', day, {
         delay: 100,
-      })
+      });
       await page.click('#tracker_time_entry_new_started_at_time', {
         clickCount: 3,
-      })
+      });
       await page.type('#tracker_time_entry_new_started_at_time', firstTime, {
         delay: 100,
-      })
+      });
 
       await page.click('#tracker_time_entry_new_ended_at_time', {
         clickCount: 3,
-      })
+      });
       await page.type('#tracker_time_entry_new_ended_at_time', lastTime, {
         delay: 100,
-      })
+      });
 
-      await page.click('input[name="commit"]', { delay: 500 })
-      console.log('Zeit gebucht für: ', day)
-      await page.waitForTimeout(1000)
+      await page.click('input[name="commit"]', { delay: 500 });
+      console.log('Zeit gebucht für: ', day);
+      await page.waitForTimeout(1000);
       await page.goto(
         correctUrl + 'zeiterfassung/buchungen?b=&show_new_form=true',
         { waitUntil: 'networkidle2' }
-      )
+      );
     }
   }
 
@@ -266,9 +266,9 @@ const getPassword = (email) =>
     'zeiterfassung/buchungen?t=' +
     firstDateForUrl +
     '..' +
-    lastDateForUrl
-  await page.waitForSelector('.logout')
-  await page.click('.logout', { delay: 1000 })
-  console.log('Logout erfolgreich. Bitte Zeiten kontrollieren: ' + controlUrl)
-  await browser.close()
-})()
+    lastDateForUrl;
+  await page.waitForSelector('.logout');
+  await page.click('.logout', { delay: 1000 });
+  console.log('Logout erfolgreich. Bitte Zeiten kontrollieren: ' + controlUrl);
+  await browser.close();
+})();
